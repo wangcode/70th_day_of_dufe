4a27c0836798db41a5297ed7de5691e8fc596b11
Wang <wang@liujia.wang>
2022-03-02 01:08:46 +0800

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -1,209 +1,208 @@
-  fileswatch = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+import pkg from 'gulp'
+const {
+  gulp,
+  src,
+  dest,
+  parallel,
+  series,
+  watch
+} = pkg
 
-  import pkg from 'gulp'
-  const {
-    gulp,
-    src,
-    dest,
-    parallel,
-    series,
-    watch
-  } = pkg
+import browserSync from 'browser-sync'
+import bssi from 'browsersync-ssi'
+import webpackStream from 'webpack-stream'
+import webpack from 'webpack'
+import TerserPlugin from 'terser-webpack-plugin'
+import gulpSass from 'gulp-sass'
+import dartSass from 'sass'
+import sassglob from 'gulp-sass-glob'
+import postCss from 'gulp-postcss'
+import cssnano from 'cssnano'
+import autoprefixer from 'autoprefixer'
+// import imagemin from 'gulp-imagemin'
+import changed from 'gulp-changed'
+import concat from 'gulp-concat'
+import rsync from 'gulp-rsync'
+import del from 'del'
+import pxtoviewport from 'postcss-px-to-viewport'
+import nunjucksRender from 'gulp-nunjucks-render'
 
-  import browserSync from 'browser-sync'
-  import bssi from 'browsersync-ssi'
-  // import ssi from 'ssi'
-  import webpackStream from 'webpack-stream'
-  import webpack from 'webpack'
-  import TerserPlugin from 'terser-webpack-plugin'
-  import gulpSass from 'gulp-sass'
-  import dartSass from 'sass'
-  import sassglob from 'gulp-sass-glob'
-  import postCss from 'gulp-postcss'
-  import cssnano from 'cssnano'
-  import autoprefixer from 'autoprefixer'
-  // import imagemin from 'gulp-imagemin'
-  import changed from 'gulp-changed'
-  import concat from 'gulp-concat'
-  import rsync from 'gulp-rsync'
-  import del from 'del'
-  import pxtoviewport from 'postcss-px-to-viewport'
-  import nunjucksRender from 'gulp-nunjucks-render'
+const fileswatch = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
 
-  const sass = gulpSass(dartSass)
+const sass = gulpSass(dartSass)
 
-  function browsersync() {
-    browserSync.init({
-      server: {
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/',
+      middleware: bssi({
         baseDir: 'app/',
-        middleware: bssi({
-          baseDir: 'app/',
-          ext: '.html'
-        })
-      },
-      ghostMode: {
-        clicks: false
-      },
-      notify: false,
-      online: true,
-      // tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
-    })
-  }
+        ext: '.html'
+      })
+    },
+    ghostMode: {
+      clicks: false
+    },
+    notify: false,
+    online: true,
+    // tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+  })
+}
 
-  function scripts() {
-    return src(['app/js/*.js', '!app/js/*.min.js'])
-      .pipe(webpackStream({
-        mode: 'production',
-        performance: {
-          hints: false
-        },
-        plugins: [
-          new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-          }), // jQuery (npm i jquery)
-        ],
-        module: {
-          rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env'],
-                plugins: ['babel-plugin-root-import']
+function scripts() {
+  return src(['app/js/*.js', '!app/js/*.min.js'])
+    .pipe(webpackStream({
+      mode: 'production',
+      performance: {
+        hints: false
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          'window.jQuery': 'jquery'
+        }), // jQuery (npm i jquery)
+      ],
+      module: {
+        rules: [{
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['babel-plugin-root-import']
+            }
+          }
+        }]
+      },
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              format: {
+                comments: false
               }
-            }
-          }]
-        },
-        optimization: {
-          minimize: true,
-          minimizer: [
-            new TerserPlugin({
-              terserOptions: {
-                format: {
-                  comments: false
-                }
-              },
-              extractComments: false
-            })
-          ]
-        },
-      }, webpack)).on('error', (err) => {
-        this.emit('end')
-      })
-      .pipe(concat('app.min.js'))
-      .pipe(dest('app/js'))
-      .pipe(browserSync.stream())
-  }
-
-  function styles() {
-    return src([`app/styles/sass/*.*`, `!app/styles/sass/_*.*`])
-      .pipe(sassglob)
-      .pipe(sass({
-        'include css': true
-      }))
-      .pipe(postCss([
-        pxtoviewport({
-          viewportWidth: 1920,
-          viewportUnit: 'vw'
-        }),
-        pxtoviewport({
-          unitToConvert: 'mpx',
-          viewportWidth: 750,
-          viewportUnit: 'vw',
-          mediaQuery: true
-        }),
-        autoprefixer({
-          grid: 'autoplace'
-        }),
-        cssnano({
-          preset: ['default', {
-            discardComments: {
-              removeAll: true
-            }
-          }]
-        })
-      ]))
-      .pipe(concat('app.min.css'))
-      .pipe(dest('app/css'))
-      .pipe(browserSync.stream())
-  }
-
-  function images() {
-    return src(['app/images/src/**/*'])
-      .pipe(changed('app/images/dist'))
-      // .pipe(imagemin())
-      .pipe(dest('app/images/dist'))
-      .pipe(browserSync.stream())
-  }
-
-  function buildcopy() {
-    return src([
-        '{app/js,app/css}/*.min.*',
-        'app/images/**/*.*',
-        '!app/images/src/**/*',
-        'app/fonts/**/*'
-      ], {
-        base: 'app/'
-      })
-      .pipe(dest('dist'))
-  }
-
-  async function html() {
-    return src('app/**/*.html')
-      .pipe(nunjucksRender({
-        path: ['app'] // String or Array
-      }))
-      .pipe(dest('dist'));
-  }
-
-  async function cleandist() {
-    del('dist/**/*', {
-      force: true
+            },
+            extractComments: false
+          })
+        ]
+      },
+    }, webpack)).on('error', (err) => {
+      this.emit('end')
     })
-  }
+    .pipe(concat('app.min.js'))
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
+}
 
-  function deploy() {
-    return src('dist/')
-      .pipe(rsync({
-        root: 'dist/',
-        hostname: 'username@yousite.com',
-        destination: 'yousite/public_html/',
-        // clean: true, // Mirror copy with file deletion
-        include: [ /* '*.htaccess' */ ], // Included files to deploy,
-        exclude: ['**/Thumbs.db', '**/*.DS_Store'],
-        recursive: true,
-        archive: true,
-        silent: false,
-        compress: true
-      }))
-  }
+function styles() {
+  return src([`app/styles/sass/*.*`, `!app/styles/sass/_*.*`])
+    .pipe(sassglob())
+    .pipe(sass({
+      'include css': true
+    }))
+    .pipe(postCss([
+      pxtoviewport({
+        viewportWidth: 1920,
+        viewportUnit: 'vw'
+      }),
+      pxtoviewport({
+        unitToConvert: 'mpx',
+        viewportWidth: 750,
+        viewportUnit: 'vw',
+        mediaQuery: true
+      }),
+      autoprefixer({
+        grid: 'autoplace'
+      }),
+      cssnano({
+        preset: ['default', {
+          discardComments: {
+            removeAll: true
+          }
+        }]
+      })
+    ]))
+    .pipe(concat('app.min.css'))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
+}
 
-  function startwatch() {
-    watch(`app/styles/sass/**/*`, {
-      usePolling: true
-    }, styles)
-    watch(['app/js/**/*.js', '!app/js/**/*.min.js'], {
-      usePolling: true
-    }, scripts)
-    watch('app/images/src/**/*', {
-      usePolling: true
-    }, images)
-    watch(`app/**/*.{${fileswatch}}`, {
-      usePolling: true
-    }).on('change', browserSync.reload)
-  }
+function images() {
+  return src(['app/images/src/**/*'])
+    .pipe(changed('app/images/dist'))
+    // .pipe(imagemin())
+    .pipe(dest('app/images/dist'))
+    .pipe(browserSync.stream())
+}
 
-  export {
-    scripts,
-    styles,
-    images,
-    html,
-    deploy
-  }
-  export let assets = series(scripts, styles, images, html)
-  export let build = series(cleandist, images, scripts, styles, buildcopy, html)
+function buildcopy() {
+  return src([
+      '{app/js,app/css}/*.min.*',
+      'app/images/**/*.*',
+      '!app/images/src/**/*',
+      'app/fonts/**/*'
+    ], {
+      base: 'app/'
+    })
+    .pipe(dest('dist'))
+}
 
-  export default series(scripts, styles, images, html, parallel(browsersync, startwatch))
+async function html() {
+  return src('app/**/*.html')
+    .pipe(nunjucksRender({
+      path: ['app'] // String or Array
+    }))
+    .pipe(dest('dist'));
+}
+
+async function cleandist() {
+  del('dist/**/*', {
+    force: true
+  })
+}
+
+function deploy() {
+  return src('dist/')
+    .pipe(rsync({
+      root: 'dist/',
+      hostname: 'username@yousite.com',
+      destination: 'yousite/public_html/',
+      // clean: true, // Mirror copy with file deletion
+      include: [ /* '*.htaccess' */ ], // Included files to deploy,
+      exclude: ['**/Thumbs.db', '**/*.DS_Store'],
+      recursive: true,
+      archive: true,
+      silent: false,
+      compress: true
+    }))
+}
+
+function startwatch() {
+  watch(`app/styles/sass/**/*`, {
+    usePolling: true
+  }, styles)
+  watch(['app/js/**/*.js', '!app/js/**/*.min.js'], {
+    usePolling: true
+  }, scripts)
+  watch('app/images/src/**/*', {
+    usePolling: true
+  }, images)
+  watch(`app/**/*.{${fileswatch}}`, {
+    usePolling: true
+  }).on('change', browserSync.reload)
+}
+
+export {
+  scripts,
+  styles,
+  images,
+  html,
+  deploy
+}
+export let assets = series(scripts, styles, images, html)
+export let build = series(cleandist, images, scripts, styles, buildcopy, html)
+
+export default series(scripts, styles, images, html, parallel(browsersync, startwatch))
