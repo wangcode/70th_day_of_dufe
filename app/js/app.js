b7cf665718275e67d5f22c6945af4a766d2312f9
Wang <wang@liujia.wang>
2022-03-04 23:09:28 +0800

    add

--- app/js/app.js
+++ app/js/app.js
@@ -4,24 +4,34 @@
 //     Fireworks
 // } from 'fireworks-js'
 // import './pages/xqdt.js'
-import Swiper from 'swiper';
+import Swiper, {
+    Pagination
+} from 'swiper';
 // import 'swiper/css/bundle';
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
     // Header
-    const header = document.querySelector('.header');
+    // const header = document.querySelector('.header');
     $(".header-mobile-menu").on("click", function () {
-        $(".header-wrap").toggle()
+        $(".header-wrap").toggleClass("open");
     })
+    // $(".header-mobile-menu").on("click", function () {
+    //     $(".header-wrap").toggle()
+    // })
     // Header
 
     // 校庆动态
-    const swiper = new Swiper('.xqdt-swiper', {
+    new Swiper('.xqdt-swiper', {
         loop: true,
+        modules: [Pagination],
         pagination: {
-            el: ".xqdt-swiper-pagination",
+            el: ".xqdt-swiper-pagination-el",
+            // bulletActiveClass: ".xqdt-swiper-pagination-active"
+        },
+        on: {
+            slideChange: console.log
         }
     })
     // 校庆动态 end
