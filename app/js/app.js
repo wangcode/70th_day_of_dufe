a670780dde509c4b3016e5971650dcfb3425b7c5
Wang <wang@liujia.wang>
2022-03-05 15:49:25 +0800

    add

--- app/js/app.js
+++ app/js/app.js
@@ -7,20 +7,86 @@
 import Swiper, {
     Pagination
 } from 'swiper';
-// import 'swiper/css/bundle';
+import {
+    Fireworks
+} from 'fireworks-js'
+
+var config = {
+    "hue": {
+        "min": 77,
+        "max": 345
+    },
+    "delay": {
+        "min": 19,
+        "max": 70
+    },
+    "rocketsPoint": {
+        "min": 29,
+        "max": 62
+    },
+    "opacity": 0.7000000000000001,
+    "acceleration": 1.02,
+    "friction": 0.96,
+    "gravity": 0.5,
+    "particles": 192,
+    "trace": 1,
+    "traceSpeed": 68,
+    "explosion": 6,
+    "intensity": 36.287374463171695,
+    "flickering": 12,
+    "lineStyle": "round",
+    "lineWidth": {
+        "explosion": {
+            "min": 2,
+            "max": 5.5
+        },
+        "trace": {
+            "min": 0,
+            "max": 0.1
+        }
+    },
+    "autoresize": true,
+    "brightness": {
+        "min": 21,
+        "max": 55,
+        "decay": {
+            "min": 0.015,
+            "max": 0.03
+        }
+    },
+    "mouse": {
+        "click": true,
+        "move": false,
+        "max": 1
+    }
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
     // Header
-    // const header = document.querySelector('.header');
     $(".header-mobile-menu").on("click", function () {
         $(".header-wrap").toggleClass("open");
     })
-    // $(".header-mobile-menu").on("click", function () {
-    //     $(".header-wrap").toggle()
-    // })
     // Header
+
+    // Welcome
+    const WelcomeSwiper = new Swiper('.welcome-swiper', {
+        loop: true
+    });
+
+    // const container = document.querySelector('.welcome-fireworks')
+    // const fireworks = new Fireworks(container, {
+    //     trace: 1,
+    //     lineWidth: {
+    //         trace: {
+    //             min: 0,
+    //             max: 0.1
+    //         }
+    //     }
+    // })
+    // fireworks.start()
+    // Welcome
 
     // 校庆动态
     new Swiper('.xqdt-swiper', {
@@ -35,4 +101,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
     // 校庆动态 end
+
+    // 校庆标识
+    new Swiper('.xqbs-postcard-swiper-el', {
+        loop: true,
+        modules: [Pagination],
+        direction: "vertical",
+        pagination: {
+            el: ".xqbs-swiper-pagination-el",
+            // bulletActiveClass: ".xqdt-swiper-pagination-active"
+        }
+    })
+    // 校庆标识
 })
