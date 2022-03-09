2b4388c838852e3f2cb058e47ac7a66f84a703ac
Wang <wang@liujia.wang>
2022-03-09 23:35:41 +0800

    add

--- app/js/app.js
+++ app/js/app.js
@@ -1,60 +1,16 @@
-// import {
-//     Fireworks
-// } from 'fireworks-js';
 import jquery from 'jquery';
 import "slick-carousel";
-// import {
-//     Fireworks
-// } from "fireworks-js";
-// window.Fireworks = Fireworks
 
 window.$ = jquery;
 window.jQuery = jquery;
 
 
-document.addEventListener('DOMContentLoaded', () => {
 
+document.addEventListener('DOMContentLoaded', () => {
     // Header
     $(".header-mobile-menu").on("click", function () {
         $(".header-wrap").toggleClass("open");
         $("body").toggleClass("noscroll")
     })
     // Header
-
-    // Welcome
-    // const WelcomeSwiper = new Swiper('.welcome-swiper', {
-    //     loop: true
-    // });
-
-    // const container = document.querySelector('.welcome-fireworks')
-    // const fireworks = new Fireworks(container, {
-    //     trace: 1,
-    //     lineWidth: {
-    //         trace: {
-    //             min: 0,
-    //             max: 0.1
-    //         }
-    //     }
-    // })
-    // fireworks.start()
-    // Welcome
-
-    // 校庆动态
-    // new Swiper('.xqdt-swiper', {
-    //     loop: true,
-    //     pagination: {
-    //         el: ".xqdt-swiper-pagination-el",
-    //     }
-    // })
-    // 校庆动态 end
-
-    // 校庆标识
-    // new Swiper('.xqbs-postcard-swiper-el', {
-    //     loop: true,
-    //     direction: "vertical",
-    //     pagination: {
-    //         el: ".xqbs-swiper-pagination-el",
-    //     }
-    // })
-    // 校庆标识
 })
